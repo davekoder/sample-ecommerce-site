b2e975c377027c77324916ac35caf165638476a2
Michael David Galibut <davekoder@gmail.com>
2023-01-08 14:41:32 -0600

    added our sign in component as well as firebase utils and brought in firebase libraries

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,66 @@
+import { initializeApp } from 'firebase/app'
+import  { 
+    getAuth, 
+    signInWithRedirect, 
+    signInWithPopup, 
+    GoogleAuthProvider
+} from 'firebase/auth'
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAoVwh1C_hK1e9tk3FqfiJUcU8Nk7cayQE",
+    authDomain: "sample-ecommerce-site-b5bef.firebaseapp.com",
+    projectId: "sample-ecommerce-site-b5bef",
+    storageBucket: "sample-ecommerce-site-b5bef.appspot.com",
+    messagingSenderId: "482853747529",
+    appId: "1:482853747529:web:7b5689f4df0b493519f898"
+ };
+  
+const firebaseApp = initializeApp(firebaseConfig)
+
+const provider = new GoogleAuthProvider() // this is just firebase auth
+// there are other google auth providers, this is the main one
+provider.setCustomParameters({
+    prompt: "select_account"
+})
+
+export const auth = getAuth(); // google implements 1 type of auth
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore() // variable db now directly points to our firebase database
+
+// this function creates a user document once a user is authenticated
+export const createUserDocumentFromAuth = async (userAuth) => {
+    // to check if there is an existing user document reference --> this reference is an 
+    // instance of a document model
+    const userDocRef = doc(db, 'users', userAuth.uid)
+    console.log(userDocRef)
+
+    const userSnapShot = await getDoc(userDocRef)
+    console.log(userSnapShot)
+    console.log(userSnapShot.exists())
+
+    if(!userSnapShot.exists()){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch (error) {
+            console.log('error creating the user', error.message)
+        }
+
+    }
+
+    return userDocRef
+}
