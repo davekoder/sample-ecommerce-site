6237706bdbe32af9ee3e9a63bb8f9bb4334e4bbe
Michael David Galibut <davekoder@gmail.com>
2023-02-22 19:06:52 -0600

    added onAuthStateChange

--- src/components/sign-in-form.component.jsx/sign-in-form.component.jsx
+++ src/components/sign-in-form.component.jsx/sign-in-form.component.jsx
@@ -1,8 +1,9 @@
 import {useState} from 'react'
-import { signInWithGooglePopup,createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
 
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component'
+
+import { signInWithGooglePopup,createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
 
 import './sign-in-form.styles.scss'
 
@@ -29,8 +30,8 @@ const SignInForm = () => {
         event.preventDefault();
 
         try {
-            const response = await signInAuthUserWithEmailAndPassword(email, password)
-            console.log(response)
+            const {user} = await signInAuthUserWithEmailAndPassword(email, password)
+            
             resetFormFields();
         }catch (error) {
             switch(error.code) {
